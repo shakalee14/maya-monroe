034f725bebdb970b0048e5c3de276664727933d2
Shakalee14 <shakalee14@gmail.com>
2017-02-21 23:15:32 -0800

    wip

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,20 @@ router.get('/home', function(request, response){
   response.render('home')
 })
 
+router.get('/maya', function(request, response){
+  response.render('maya')
+})
+
+router.get('/etiquette', function(request, response){
+  response.render('etiquette')
+})
+
+router.get('/sessions', function(request, response){
+  response.render('sessions')
+})
+
+router.get('/contact', function(request, response){
+  response.render('contact')
+})
+
 module.exports = router
