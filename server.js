bee924d294bf1b7cc22b617f5cf342b22ad8e201
Shakalee14 <shakalee14@gmail.com>
2017-02-21 21:46:56 -0800

    initialized project

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var pug = require('pug');
+
+var index = require('./routes/index');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+app.listen('2626', function(){
+  console.log('listening on port 2626')
+})
+
+module.exports = app;
+
